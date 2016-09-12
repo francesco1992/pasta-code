9ce07d08304f0223200bfa82b88401ff5b1c6b01
fb920024 <francesco.benedetto263@gmail.com>
2016-09-12 17:16:07 +0200

    Statistics and settings pages added

---
+++ app/pages/statistics/statistics.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the StatisticsPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/statistics/statistics.html',
+})
+export class StatisticsPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
