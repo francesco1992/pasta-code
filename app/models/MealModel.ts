60b46c768a4d2d87dc1f0ab9ff6675b1a00c6cf1
fb920024 <francesco.benedetto263@gmail.com>
2016-09-12 17:34:55 +0200

    Meal model added. Home page modified to list all meals

---
+++ app/models/MealModel.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by francesco on 12/09/2016.
+ */
+export class MealModel {
+  name: string;
+  count: number;
+
+  constructor(name: string, count: number) {
+    this.name = name;
+    this.count = count;
+  }
+}
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,54 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, AlertController} from 'ionic-angular';
+import {MealModel} from "../../models/MealModel";
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private navCtrl: NavController) {
-  
+  private meals: MealModel[] = [];
+  private currentDate: string;
+
+  constructor(private navCtrl: NavController, private alertCtrl: AlertController) {
+    this.currentDate = new Date().toLocaleDateString();
+    this.meals.push({name: "test", count: 0});
+  }
+
+  addMeal() {
+
+  }
+
+  deleteMeal(event, meal) {
+    let alert = this.alertCtrl.create({
+      title: 'Delete Meal',
+      message: 'Do you want to delete <i>'+meal.name+"</i>?",
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => { }
+        },
+        {
+          text: 'Go!',
+          handler: () => {
+            var index = this.meals.indexOf(meal, 0);
+            if (index > -1) {
+              this.meals.splice(index, 1);
+            }
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  increaseCount(meal) {
+    meal.count++;
+  }
+
+  decreaseCount(meal) {
+    if(meal.count > 0) {
+      meal.count--;
+    }
   }
 }
